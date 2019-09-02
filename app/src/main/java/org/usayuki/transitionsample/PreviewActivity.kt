285710aa32d7870d51a722b5522532889a1a1097
usayuki <masapp.01a@gmail.com>
2019-09-02 18:46:21 +0900

    add layout

---
+++ app/src/main/java/org/usayuki/transitionsample/PreviewActivity.kt
@@ -0,0 +1,12 @@
+package org.usayuki.transitionsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
