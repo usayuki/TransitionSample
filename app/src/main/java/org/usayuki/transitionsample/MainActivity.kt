a338283d0a5124e1fead0f6fd5d6e24839fd97e5
usayuki <masapp.01a@gmail.com>
2019-09-03 14:02:36 +0900

    add transition

--- app/src/main/java/org/usayuki/transitionsample/MainActivity.kt
+++ app/src/main/java/org/usayuki/transitionsample/MainActivity.kt
@@ -1,12 +1,30 @@
 package org.usayuki.transitionsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView1 = findViewById(R.id.imageView1) as ImageView
+        imageView1.setOnClickListener {
+            nextView(R.drawable.cat)
+        }
+
+        val imageView2 = findViewById(R.id.imageView2) as ImageView
+        imageView2.setOnClickListener {
+            nextView(R.drawable.tarot)
+        }
+    }
+
+    private fun nextView(image: Int) {
+        val intent = Intent(this, PreviewActivity::class.java)
+        intent.putExtra("image", image)
+        startActivity(intent)
     }
 }
--- app/src/main/java/org/usayuki/transitionsample/PreviewActivity.kt
+++ app/src/main/java/org/usayuki/transitionsample/PreviewActivity.kt
@@ -2,11 +2,22 @@ package org.usayuki.transitionsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class PreviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+
+        val image = intent.getIntExtra("image", R.drawable.cat)
+        val imageView = findViewById(R.id.imageView) as ImageView
+        imageView.setImageResource(image)
+
+        val backButton = findViewById(R.id.button) as Button
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
